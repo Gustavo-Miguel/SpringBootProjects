ae0cc6ef862c4ed11f7fb69bb865e36e5581f392
Gustavo Miguel <gustavo_alves_miguel@hotmail.com>
2019-05-29 23:03:17 -0300

    Inicializando projeto spring, zero à api

---
+++ MiguelProject/src/main/java/com/miguel/first/MiguelProjectApplication.java
@@ -0,0 +1,13 @@
+package com.miguel.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiguelProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiguelProjectApplication.class, args);
+	}
+
+}
