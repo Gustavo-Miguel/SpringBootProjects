681ae13e732a2d128bff0640a5595afeb13fd571
Gustavo Miguel <gustavo_alves_miguel@hotmail.com>
2021-04-25 01:30:22 -0300

    Authorization implementation

---
+++ MiguelProject/src/main/java/com/miguel/project/security/AccountCredentials.java
@@ -0,0 +1,37 @@
+package com.miguel.project.security;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AccountCredentials {
+
+    private String username;
+    private String password;
+    private String token;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
