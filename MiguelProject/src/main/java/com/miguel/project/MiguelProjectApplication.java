840371ab610c7ab9d7755c50c9394a884ed003b1
Gustavo Miguel <33351250+Gustavo-Miguel@users.noreply.github.com>
2023-08-29 15:11:58 -0300

    Create a dependency jwt authentication

--- MiguelProject/src/main/java/com/miguel/project/MiguelProjectApplication.java
+++ MiguelProject/src/main/java/com/miguel/project/MiguelProjectApplication.java
@@ -1,10 +1,14 @@
 package com.miguel.project;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan(basePackages = {"org.project_utils.config.security", "com.miguel.project"})
 @EnableJpaRepositories("com.miguel.project.model.repository")
 public class MiguelProjectApplication {
 
